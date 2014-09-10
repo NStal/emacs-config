4077c44dadd104ba966827bcd16aa2f83883b495
NStal <nstalmail@gmail.com>
2014-09-10 16:02:48 +0800

    initial

---
+++ custom-function.el
@@ -0,0 +1,89 @@
+(defun create-pair (a b)
+  (interactive)
+  (progn
+    (insert-string a)
+    (insert-string b)
+    (backward-char (length b))))
+(defmacro gen-pair-handler (a b)
+  (list 'lambda (list) (list 'interactive)
+	(list 'create-pair a b)))
+(global-set-key (kbd "M-\"") (gen-pair-handler "\"" "\""))
+(global-set-key (kbd "M-(") (gen-pair-handler "(" ")"))
+(global-set-key (kbd "M-{") (gen-pair-handler "{" "}"))
+(global-set-key (kbd "M-{") (gen-pair-handler "{" "}"))
+(provide 'custom-function)
+
+
+(defun npm-install (name)
+  (interactive "MPackageName:\n")
+  (progn
+    (with-output-to-temp-buffer "*npm-install-buffer*"
+      (switch-to-buffer "*npm-install-buffer*")
+      (defun ansi-color-filter (proc string)
+                                     (when (buffer-live-p (process-buffer proc))
+                                       (with-current-buffer (process-buffer proc)
+                                         
+                                         (read-only-mode -1)
+                                         (let ((moving (= (point) (process-mark proc))))
+                                           (save-excursion
+                                             ;; Insert the text, advancing the process marker.
+                                             (goto-char (process-mark proc))
+                                             (insert (ansi-color-apply string))
+                                             (set-marker (process-mark proc) (point)))
+                                           (if moving (goto-char (process-mark proc)))))))
+      
+      (set-process-filter 
+       (start-process-shell-command "emacs-npm-install" (current-buffer) (concat "npm install " name))
+       (function ansi-color-filter))
+    )
+    ))
+
+(defun nodejs-export-selected-var-as-module ()
+  """export the selected vard as module.exports = xxx"""
+  (interactive)
+  (progn 
+    (save-excursion
+      (let ((name (buffer-substring (region-beginning) (region-end))))
+            (goto-char (point-max))
+            (insert "\n")
+            (insert (concat "module.exports = " name))
+            )
+      )
+    )
+  )
+
+(defun nodejs-export-selected-var ()
+  """export the selected vard"""
+  (interactive)
+  (progn 
+    (save-excursion
+      (let ((name (buffer-substring (region-beginning) (region-end))))
+            (goto-char (point-max))
+            (insert "\n")
+            (insert (concat "module.exports." name " = " name))
+            )
+      )
+    )
+  )
+(defun coffee-global-require (name var)
+  """globally require the package"""
+  (interactive "Mrequired package name: \nMvar name:\n")
+  (progn
+    (save-excursion
+      (goto-char (point-min))
+      (insert (concat var " = " "require(\"" name "\")\n"))
+      ))
+  )
+(add-hook 'coffee-mode-hook
+          (lambda ()
+            
+            (global-set-key (kbd "C-c p") 'nodejs-export-selected-var)
+            (global-set-key (kbd "C-c C-p") 'nodejs-export-selected-var-as-module)
+            (global-set-key (kbd "C-c g") 'coffee-global-require)
+            ))
+(add-hook 'javascript-mode-hook
+          (lambda (
+                   (global-set-key (kbd "C-c p") 'nodejs-export-selected-var)
+                   (global-set-key (kbd "C-c C-p") 'nodejs-export-selected-var-as-module)
+                   ))
+          )
---
+++ main.el
@@ -0,0 +1,20 @@
+(add-hook 'go-mode-hook
+	  (lambda ()
+	    (setq indent-tabs-mode t)
+	    (setq tab-width 4)
+	    (setq python-indent 4)))
+(require 'custom-function)
+(require 'custom-preference)
+(require 'custom-plugin-load)
+(require 'custom-hotkey)
+
+(require 'package)
+;; Add the original Emacs Lisp Package Archive
+(add-to-list 'package-archives
+             '("elpa" . "http://tromey.com/elpa/"))
+;; Add the user-contributed repository
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(setq-default indent-tabs-mode nil)
+
+(provide 'main)
