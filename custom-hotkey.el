1ea4c51ed22646a04171034ed7c77a1a12d24a83
NStal <nstalmail@gmail.com>
2014-10-22 17:19:58 +0800

    add select-string-literal relates

--- custom-hotkey.el
+++ custom-hotkey.el
@@ -3,4 +3,6 @@
 (global-set-key [(control c) (control g)] 'goto-line)
 (global-set-key [(control c) (control s)] 'replace-string)
 (global-set-key [(control c) (m)] 'magit-status)
+(global-set-key [(control c) (s) (l)] 'select-current-string-literal)
+(global-set-key [(control c) (s) (k)] 'select-current-string-literal-content)
 (provide 'custom-hotkey)
